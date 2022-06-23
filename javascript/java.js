95fb5261508430d07c9ec6ae67a1980b254f2539
Elias Fernandes <eliasneto0014@gmail.com>
2022-06-23 12:03:40 -0300

    First

---
+++ javascript/java.js
@@ -0,0 +1,8 @@
+document.getElementById("menu-btn").addEventListener("click",function(){
+    document.querySelector(".navbar").style.display = "flex";
+})
+
+document.getElementById("close-login-btn").addEventListener("click", function(){
+    document.querySelector(".login-form-container").style.display = "none";
+})
+
