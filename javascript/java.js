cf25249d0632fe900ab94384d417f03de82f554c
Elias Fernandes <eliasneto0014@gmail.com>
2022-06-24 14:03:39 -0300

    commit 2

--- javascript/java.js
+++ javascript/java.js
@@ -1,8 +1,2 @@
-document.getElementById("menu-btn").addEventListener("click",function(){
-    document.querySelector(".navbar").style.display = "flex";
-})
 
-document.getElementById("close-login-btn").addEventListener("click", function(){
-    document.querySelector(".login-form-container").style.display = "none";
-})
 
