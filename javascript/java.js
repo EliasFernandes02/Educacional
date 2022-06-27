5cac2de99b000effcfeade7ec00a4de9758c5380
Elias Fernandes <eliasneto0014@gmail.com>
2022-06-27 13:18:14 -0300

    commit 3

--- javascript/java.js
+++ javascript/java.js
@@ -1,2 +1,54 @@
+var swiper = new Swiper(".course-slider", {
+    spaceBetween:20,
+    grabCursos:true,
+    loop:true,
+    pagination: {
+
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+     breakpoints: {
+        640: {
+          slidesPerView: 1,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
+
+  var swiper = new Swiper(".teachers-slider", {
+    spaceBetween:20,
+    grabCursos:true,
+    loop:true,
+    pagination: {
+
+      el: ".swiper-pagination",
+      clickable:true,
+    },
+     breakpoints: {
+        640: {
+          slidesPerView: 1,
+          
+        },
+        768: {
+          slidesPerView: 2,
+          
+        },
+        1024: {
+          slidesPerView: 3,
+          
+        },
+      },
+  });
 
 
+
+
+ 
